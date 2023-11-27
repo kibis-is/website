8b4e249079ccdb779e344c586bba997068bbe648
Kieran O'Neill <kieran.oneill@pm.me>
2023-11-27 18:57:40 +0000

    feat: add attribution to header image

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,27 +10,42 @@ import styles from './styles.module.scss';
 const Header: FC = () => {
   return (
     <header className={styles.wrapper}>
-      {/*title*/}
-      <div className={clsx(styles.container, styles['text-container'])}>
-        <h1 className={clsx(styles.text, styles['text--title'])}>
-          The AVM utility wallet
-        </h1>
+      <div className={styles['content-container']}>
+        {/*title*/}
+        <div className={clsx(styles.container, styles['text-container'])}>
+          <h1 className={clsx(styles.text, styles['text--title'])}>
+            The AVM utility wallet
+          </h1>
 
-        <p className={clsx(styles.text, styles['text--subtitle'])}>
-          A wallet that is not just for DeFi, but also focuses on utility.
-        </p>
+          <p className={clsx(styles.text, styles['text--subtitle'])}>
+            A wallet that is not just for DeFi, but also focuses on utility.
+          </p>
+        </div>
+
+        {/*image*/}
+        <div className={clsx(styles.container, styles['image-container'])}>
+          <ThemedImage
+            alt="Wallet home screen"
+            className={styles.image}
+            sources={{
+              dark: '/images/home_screen-dark.png',
+              light: '/images/home_screen-light.png',
+            }}
+          />
+        </div>
       </div>
 
-      {/*image*/}
-      <div className={clsx(styles.container, styles['image-container'])}>
-        <ThemedImage
-          alt="Wallet home screen"
-          className={styles.image}
-          sources={{
-            dark: '/images/home_screen-dark.png',
-            light: '/images/home_screen-light.png',
-          }}
-        />
+      {/*attribution*/}
+      <div className={styles['attribution-container']}>
+        <p className={clsx(styles.text, styles['text--attribution'])}>
+          Image by{` `}
+          <a
+            href="https://www.freepik.com/free-vector/gradient-geometric-background_12283006.htm"
+            target="_blank"
+          >
+            Freepik
+          </a>
+        </p>
       </div>
     </header>
   );
