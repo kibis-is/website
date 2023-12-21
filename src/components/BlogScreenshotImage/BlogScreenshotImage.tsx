49326b8cace80347b7ae9f0f9bcd95d3ecb50411
Kieran O'Neill <hello@kieranoneill.com>
2023-12-21 16:53:59 +0000

    feat: write blog post for chrome extension release (#17)
    * feat: add new blog post to introduce kibisis
    
    * docs: update kieran author
    
    * feat: add new component to display screensshots and add screenshots to post

---
+++ src/components/BlogScreenshotImage/BlogScreenshotImage.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+
+// components
+import ThemedImage from '@theme/ThemedImage';
+
+// styles
+import styles from './styles.module.scss';
+
+// types
+import { IThemedImageSources } from '@site/src/types';
+
+interface IProps {
+  label: string;
+  maxWidth?: string;
+  imageSources: IThemedImageSources;
+}
+
+const BlogScreenshotImage: FC<IProps> = ({
+  label,
+  maxWidth = '400px',
+  imageSources,
+}: IProps) => {
+  return (
+    <div className={styles.container}>
+      <ThemedImage
+        alt={label}
+        aria-label={label}
+        className={styles.image}
+        sources={imageSources}
+        style={{
+          maxWidth,
+        }}
+      />
+    </div>
+  );
+};
+
+export default BlogScreenshotImage;
