f120bfec4c2e869e178c382a92e20a3fb4cabf9b
Kieran O'Neill <kieran.oneill@pm.me>
2023-12-24 19:34:51 +0000

    feat: add version 1.8.0 blog post with arc200 asset screens

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -1,2 +1,11 @@
+export const AGORA_LABS_LINK: string = 'https://agoralabs.sh';
 export const CHROME_STORE_LINK: string =
   'https://chromewebstore.google.com/detail/kibisis/hcgejekffjilpgbommjoklpneekbkajb';
+export const GITHUB_LINK: string =
+  'https://github.com/agoralabs-sh/kibisis-web-extension';
+export const KIBISIS_LINK: string = 'https://kibis.is';
+export const REPORT_A_BUG_LINK: string =
+  'https://github.com/agoralabs-sh/kibisis-web-extension/issues/new?assignees=kieranroneill&labels=%F0%9F%90%9B+bug&projects=&template=bug_report_template.yml&title=%5BBug%5D%3A+';
+export const SUGGEST_A_FEATURE_LINK: string =
+  'https://github.com/agoralabs-sh/kibisis-web-extension/issues/new?assignees=&labels=%E2%9C%A8+feature&projects=&template=feature_request_template.yml&title=%5BFeature%5D%3A+';
+export const X_LINK: string = 'https://x.com/kibisis_wallet';
