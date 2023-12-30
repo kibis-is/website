e82ff192b1a6ccf22657c1b33f55c6e9954733ad
Kieran O'Neill <kieran.oneill@pm.me>
2023-12-30 15:21:23 +0000

    fix: centered image max width applies to inner container not image

--- src/components/CenteredImage/CenteredImage.tsx
+++ src/components/CenteredImage/CenteredImage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 // styles
 import styles from './styles.module.scss';
+import clsx from 'clsx';
 
 interface IProps {
   label: string;
@@ -9,17 +10,21 @@ interface IProps {
   src: string;
 }
 
-const CenteredImage: FC<IProps> = ({ label, maxWidth, src }: IProps) => {
+const CenteredImage: FC<IProps> = ({
+  label,
+  maxWidth = '400px',
+  src,
+}: IProps) => {
   return (
-    <div className={styles.container}>
-      <img
-        alt={label}
-        aria-label={label}
-        src={src}
+    <div className={clsx(styles.container, styles.container__outer)}>
+      <div
+        className={styles.container}
         style={{
           maxWidth,
         }}
-      />
+      >
+        <img alt={label} aria-label={label} src={src} />
+      </div>
     </div>
   );
 };
