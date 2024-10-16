d69f8808fb2974de8232227f704382377a9981f1
Kieran O'Neill <hello@kieranoneill.com>
2024-10-16 09:50:49 +0100

    feat: update agoralabs references to use kibis github org and repos

--- src/constants/Links.ts
+++ src/constants/Links.ts
@@ -5,11 +5,10 @@ export const EDGE_STORE_LINK: string =
   'https://microsoftedge.microsoft.com/addons/detail/kibisis/bajncpocmkioafbijldokfbjajelkbmc';
 export const FIREFOX_ADD_ON_LINK: string =
   'https://addons.mozilla.org/addon/kibisis/';
-export const GITHUB_LINK: string =
-  'https://github.com/agoralabs-sh/kibisis-web-extension';
+export const GITHUB_LINK: string = 'https://github.com/kibis-is/web-extension';
 export const KIBISIS_LINK: string = 'https://kibis.is';
 export const REPORT_A_BUG_LINK: string =
-  'https://github.com/agoralabs-sh/kibisis-web-extension/issues/new?assignees=kieranroneill&labels=%F0%9F%90%9B+bug&projects=&template=bug_report_template.yml&title=%5BBug%5D%3A+';
+  'https://github.com/kibis-is/web-extension/issues/new?assignees=kieranroneill&labels=%F0%9F%90%9B+bug&projects=&template=bug_report_template.yml&title=%5BBug%5D%3A+';
 export const SUGGEST_A_FEATURE_LINK: string =
-  'https://github.com/agoralabs-sh/kibisis-web-extension/issues/new?assignees=&labels=%E2%9C%A8+feature&projects=&template=feature_request_template.yml&title=%5BFeature%5D%3A+';
+  'https://github.com/kibis-is/web-extension/issues/new?assignees=&labels=%E2%9C%A8+feature&projects=&template=feature_request_template.yml&title=%5BFeature%5D%3A+';
 export const X_LINK: string = 'https://x.com/kibisis_wallet';
